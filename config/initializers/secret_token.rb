90725db2499e59f4dddf7fc252fff5d325a606ed
scige <panf06@gmail.com>
2012-09-09 16:14:47 +0800

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sanbaoyuan::Application.config.secret_token = 'a8be75c7d2376450b1009280a3bcb32b99652ef8ba8ab84742d9f7016ea945fb40b85ad8ac7740c56030565e9f9f0ba0f10b1ca430e42281a8567798a492675c'
